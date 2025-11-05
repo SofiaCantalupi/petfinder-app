468b7f092ea5183ebedd14189c63d4e8a38d9689
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-05 14:06:03 -0300

    Borrar imports sin uso

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MuroPublicaciones } from './pages/muro-publicaciones/muro-publicaciones';
+import { PublicacionFormComponent } from './pages/publicacion-form-component/publicacion-form-component';
+
 
 @Component({
   selector: 'app-root',
