a764eb50bc2131e6db1f7d6ce7c3e16493f9745a
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-04 21:22:23 -0300

    Creacion de PublicacionService, junto a metodos put, get, delete y post

---
+++ src/app/services/publicacion-service.ts
@@ -0,0 +1,61 @@
+import { Injectable, signal } from '@angular/core';
+import { Publicacion } from '../../models/publicacion.model';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PublicacionService {
+  private readonly apiUrl = 'http://localhost:3000/publicaciones';
+
+  private publicacionesState = signal<Publicacion[]>([]);
+
+  public publicaciones = this.publicacionesState.asReadonly();
+
+  constructor(private http: HttpClient) {
+    this.getPublicaciones();
+  }
+
+  getPublicaciones() {
+    this.http.get<Publicacion[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.publicacionesState.set(data);
+      },
+      error: (error) => {
+        console.log('Error al obtener publicaciones', error);
+      },
+    });
+  }
+
+  postPublicacion(nuevaPublicacion: Omit<Publicacion, 'id'>) {
+    this.http
+      .post<Publicacion>(this.apiUrl, nuevaPublicacion)
+      .pipe(
+        tap((data) => {
+          this.publicacionesState.update((publicaciones) => [...publicaciones, data]);
+        })
+      )
+      .subscribe();
+  }
+
+  putPublicacion(id: number, nuevaPublicacion: Omit<Publicacion, 'id'>) {
+    return this.http.put<Publicacion>(`${this.apiUrl}/${id}`, nuevaPublicacion).pipe(
+      tap((data) => {
+        this.publicacionesState.update((publicaciones) =>
+          publicaciones.map((pub) => (pub.id === id ? data : pub))
+        );
+      })
+    );
+  }
+
+  deletePublicacion(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        this.publicacionesState.update((publicaciones) =>
+          publicaciones.filter((pub) => pub.id !== id)
+        );
+      })
+    );
+  }
+}
