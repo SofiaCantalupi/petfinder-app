04ee6da52580ed9309e0195b8dd10a5a624e9aba
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-04 21:21:37 -0300

    Crear interfaz Publicacion

---
+++ src/models/publicacion.model.ts
@@ -0,0 +1,11 @@
+export interface Publicacion{
+    id: number,
+    nombreMascota: string,
+    tipoMascota: string,
+    estadoMascota: string,
+    urlFoto: string,
+    fecha: Date,
+    descripcion: string,
+    calle: string,
+    altura: number
+}
