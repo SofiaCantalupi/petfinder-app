b28e5d0a0bcf8ad68d87bab901d315e10289209a
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-05 14:02:11 -0300

    Crear componente publicacion-detail

---
+++ src/app/pages/publicacion-detail/publicacion-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacion-detail',
+  imports: [],
+  templateUrl: './publicacion-detail.html',
+  styleUrl: './publicacion-detail.css',
+})
+export class PublicacionDetail {
+
+}
