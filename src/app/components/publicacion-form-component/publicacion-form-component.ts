68c831f4807440a0f614e958067c463b9b8a45e3
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-04 21:23:08 -0300

    Cambiar form anidado a form plano. Inyeccion de PublicacionService

--- src/app/components/publicacion-form-component/publicacion-form-component.ts
+++ src/app/components/publicacion-form-component/publicacion-form-component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { PublicacionService } from '../../services/publicacion-service';
 
 @Component({
   selector: 'app-publicacion-form-component',
@@ -10,10 +11,14 @@ import { Validators } from '@angular/forms';
 })
 export class PublicacionFormComponent {
   private formBuilder = inject(FormBuilder);
+  private publicacionService = inject(PublicacionService);
 
   // input para saber si estamos editando CAMBIAR POR SIGNAL
-  @Input() esEdicion = false;
-  @Input() publicacionId?: number; // si viene un ID, estamos editando
+  /* @Input() esEdicion = false;
+  @Input() publicacionId?: number; // si viene un ID, estamos editando */
+
+  esEdicion = signal(false);
+  publicacionId = signal<number | undefined>(undefined);
 
   // Arrays para las opciones
   estadosMascota = [
@@ -21,10 +26,10 @@ export class PublicacionFormComponent {
     { value: 'encontrado', label: 'Encontrado' },
     { value: 'reencontrado', label: 'Reencontrado' },
   ];
-  
+
   // filtra el estado "reencontrado" si estamos creando una publicacion
   get estadosMascotaFiltrados() {
-    if (this.esEdicion) {
+    if (this.esEdicion()) {
       return this.estadosMascota;
     }
     return this.estadosMascota.filter((e) => e.value !== 'reencontrado');
@@ -35,25 +40,30 @@ export class PublicacionFormComponent {
     { value: 'gato', label: 'Gato' },
   ];
 
-  publicacionForm = this.formBuilder.group({
-    mascota: this.formBuilder.group({
-      nombreMascota: ['', [Validators.required, Validators.minLength(3)]],
-      estadoMascota: ['', Validators.required],
-      tipoMascota: ['', Validators.required],
-      urlFoto: ['', [Validators.required, Validators.pattern(/^https?:\/\/.+/)]],
-    }),
-    descripcion: ['', [Validators.required, Validators.minLength(3)]],
-    ubicacion: this.formBuilder.group({
-      calle: ['', [Validators.required, Validators.minLength(3)]],
-      altura: [0, [Validators.required, Validators.min(1)]],
-    }),
+  publicacionForm = this.formBuilder.nonNullable.group({
+    nombreMascota: ['', [Validators.required, Validators.minLength(3)]],
+    tipoMascota: ['', Validators.required],
+    estadoMascota: ['', Validators.required],
+    urlFoto: ['', [Validators.required, Validators.pattern(/^https?:\/\/.+/)]],
+    descripcion: ['', [Validators.required, Validators.minLength(10)]],
+    calle: ['', [Validators.required, Validators.minLength(3)]],
+    altura: [0, [Validators.required, Validators.min(1)]],
   });
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.publicacionForm.valid) {
+      const publicacion = {
+        ...this.publicacionForm.getRawValue(),
+        fecha: new Date(),
+      };
+      this.publicacionService.postPublicacion(publicacion);
+      this.publicacionForm.reset();
+    }
+  }
+}
 
-  /*  <!-- Para crear -->
+/*  <!-- Para crear -->
 <app-publicacion-form></app-publicacion-form>
 
 <!-- Para editar -->
 <app-publicacion-form [esEdicion]="true" [publicacionId]="123"></app-publicacion-form> */
-}
