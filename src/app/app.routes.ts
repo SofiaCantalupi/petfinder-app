bde9f5d9ca6e598d7105c1ef15aa16f1a70214dc
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-05 14:06:20 -0300

    Agregar configuracion de rutas en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { MuroPublicaciones } from './pages/muro-publicaciones/muro-publicaciones';
+import { PublicacionFormComponent } from './pages/publicacion-form-component/publicacion-form-component';
+import { PublicacionDetail } from './pages/publicacion-detail/publicacion-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', 
+        redirectTo: 'publicaciones/crear',
+        pathMatch: 'full'
+    },
+    {
+        path: 'publicaciones',
+        component: MuroPublicaciones
+    },
+    {
+        path: 'publicaciones/crear',
+        component: PublicacionFormComponent
+    },
+    {
+        path: 'publicaciones/:id',
+        component: PublicacionDetail
+    },
+    {
+        path: 'publicaciones/:id/editar',
+        component: PublicacionFormComponent
+    }
+];
