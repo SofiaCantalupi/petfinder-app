ca752ef37cfd36f59dc6e8a91d2130e3adc9b566
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-09 19:49:22 -0300

    delete unuse imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MuroPublicaciones } from './pages/muro-publicaciones/muro-publicaciones';
-import { PublicacionFormComponent } from './pages/publicacion-form-component/publicacion-form-component';
-
 
 @Component({
   selector: 'app-root',
