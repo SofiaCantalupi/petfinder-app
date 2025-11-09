f11f92334db5093e808a77b0e02fc03dfc819020
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-09 19:46:46 -0300

    create guia-estilo component

---
+++ src/app/prueba/guia-estilo/guia-estilo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guia-estilo',
+  imports: [],
+  templateUrl: './guia-estilo.html',
+  styleUrl: './guia-estilo.css',
+})
+export class GuiaEstilo {
+
+}
