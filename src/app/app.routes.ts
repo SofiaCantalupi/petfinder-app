09dde53e4592177863483edbe5bb42cbd620a7fb
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-10 09:17:09 -0300

    add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,32 @@ import { Routes } from '@angular/router';
 import { MuroPublicaciones } from './pages/muro-publicaciones/muro-publicaciones';
 import { PublicacionFormComponent } from './pages/publicacion-form-component/publicacion-form-component';
 import { PublicacionDetail } from './pages/publicacion-detail/publicacion-detail';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-    {
-        path: '', 
-        redirectTo: 'publicaciones/crear',
-        pathMatch: 'full'
-    },
-    {
-        path: 'publicaciones',
-        component: MuroPublicaciones
-    },
-    {
-        path: 'publicaciones/crear',
-        component: PublicacionFormComponent
-    },
-    {
-        path: 'publicaciones/:id',
-        component: PublicacionDetail
-    },
-    {
-        path: 'publicaciones/:id/editar',
-        component: PublicacionFormComponent
-    }
+  {
+    path: '',
+    redirectTo: 'publicaciones/crear',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'publicaciones',
+    component: MuroPublicaciones,
+  },
+  {
+    path: 'publicaciones/crear',
+    component: PublicacionFormComponent,
+  },
+  {
+    path: 'publicaciones/:id',
+    component: PublicacionDetail,
+  },
+  {
+    path: 'publicaciones/:id/editar',
+    component: PublicacionFormComponent,
+  },
 ];
