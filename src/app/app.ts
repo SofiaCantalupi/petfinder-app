c56598fe35029161b57214843efcb9caaa84efbf
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-10 09:18:50 -0300

    add body and main tag

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GuiaEstilo } from './prueba/guia-estilo/guia-estilo';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GuiaEstilo, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
