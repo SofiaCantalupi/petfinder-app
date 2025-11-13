1dd12b730ddd983e5e855f54465ad56631434f78
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-13 19:31:30 -0300

    import guia-estilo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { GuiaEstilo } from './prueba/guia-estilo/guia-estilo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, GuiaEstilo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
