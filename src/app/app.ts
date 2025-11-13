87b2de49541ca2fc4f0d3a4eb63f21a6de7c22fb
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-13 19:09:00 -0300

    add footer into app.ts imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GuiaEstilo } from './prueba/guia-estilo/guia-estilo';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GuiaEstilo, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
