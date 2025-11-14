f86260e687ada2498cfcfff8208b28ad928b3525
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-14 14:52:18 -0300

    add injects, ngOnInit, navigate and delete methods into publicacion-detail component

--- src/app/pages/publicacion-detail/publicacion-detail.ts
+++ src/app/pages/publicacion-detail/publicacion-detail.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { PublicacionService } from '../../services/publicacion-service';
+import { Publicacion } from '../../models/publicacion';
 
 @Component({
   selector: 'app-publicacion-detail',
@@ -7,6 +9,48 @@ import { RouterLink } from "@angular/router";
   templateUrl: './publicacion-detail.html',
   styleUrl: './publicacion-detail.css',
 })
-export class PublicacionDetail {
+export class PublicacionDetail implements OnInit {
+  publicacionService = inject(PublicacionService);
+  route = inject(ActivatedRoute);
+  router = inject(Router);
 
+  selectedPublicacion = signal<Publicacion | null>(null);
+
+  ngOnInit(): void {
+    const idPublicacion = this.route.snapshot.params['id'];
+
+    this.publicacionService.getPublicacionById(idPublicacion).subscribe({
+      next: (data) => {
+        this.selectedPublicacion.set(data);
+      },
+      error: (error) => {
+        console.log('Error al obtener la publicacion por ID', error);
+      }
+    });
+  }
+
+  navigateToUpdate(){
+    const id = this.selectedPublicacion()?.id;
+    this.router.navigate(['/publicaciones', id, 'editar']);
+  }
+
+  deletePublicacion(){
+    const publicacion = this.selectedPublicacion();
+
+    if(!publicacion){
+      return;
+    }
+
+    if(confirm('Estas seguro que queres eliminar esta publicación?')){
+      this.publicacionService.deletePublicacion(publicacion.id).subscribe({
+        next: () => {
+          console.log('Publicación eliminada.');
+          this.router.navigate(['/publicaciones']); // volver al muro de publicaciones
+        }, 
+        error: (error) => {
+          console.log('Error al eliminar la publicación.', error);
+        }
+      });
+    }
+  }
 }
