d144127346811831a50d5345a8c1f3e514cc3464
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-16 19:54:48 -0300

    apply style to publication-card

--- src/app/components/publicacion-card/publicacion-card.ts
+++ src/app/components/publicacion-card/publicacion-card.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Publicacion } from '../../models/publicacion';
+import { RouterLink } from "@angular/router";
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-publicacion-card',
-  imports: [],
+  imports: [RouterLink, DatePipe, NgClass],
   templateUrl: './publicacion-card.html',
   styleUrl: './publicacion-card.css',
 })
