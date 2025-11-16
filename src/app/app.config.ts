a6c5e9d95c3a2bda8e947c32a1687974a502e94d
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-16 20:50:24 -0300

    register local data like spanish

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// se regista el locale es espaniol
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,11 +21,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       /* esto permite que cada vez que se navegue, te lleve a la parte de arriba de la pagina */
       routes,
-       withInMemoryScrolling({
+      withInMemoryScrolling({
         scrollPositionRestoration: 'top',
-        anchorScrolling: 'enabled'
-       })
-    ), 
-    provideHttpClient()
-  ]
+        anchorScrolling: 'enabled',
+      })
+    ),
+    { provide: LOCALE_ID, useValue: 'es' },
+    provideHttpClient(),
+  ],
 };
