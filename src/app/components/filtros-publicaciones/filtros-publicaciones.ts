77c75156ed23a59aa98f37a7be19cec995a670c9
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-16 21:14:43 -0300

    create filtros-publicaciones component

---
+++ src/app/components/filtros-publicaciones/filtros-publicaciones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtros-publicaciones',
+  imports: [],
+  templateUrl: './filtros-publicaciones.html',
+  styleUrl: './filtros-publicaciones.css',
+})
+export class FiltrosPublicaciones {
+
+}
