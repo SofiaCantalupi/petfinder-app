4447e8e4172c9dfb5162e2dd36bdb58f9bca7785
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-20 16:20:15 -0300

    add update pet state method

--- src/app/services/publicacion-service.ts
+++ src/app/services/publicacion-service.ts
@@ -1,9 +1,8 @@
 import { Injectable, signal, computed } from '@angular/core';
-import { Publicacion } from '../models/publicacion';
+import { EstadoMascota, Publicacion } from '../models/publicacion';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs';
 import { switchMap } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root',
@@ -16,12 +15,14 @@ export class PublicacionService {
   public publicaciones = this.publicacionesState.asReadonly();
 
   // computed usado para filtrar publicaciones activas, filtra solo cuando hay cambios
-  public publicacionesActivas = computed(()=>
-    this.publicacionesState().filter(publicacion => publicacion.activo === true)
+  public publicacionesActivas = computed(() =>
+    this.publicacionesState().filter((publicacion) => publicacion.activo === true)
   );
 
-  public publicacionesReencontrados = computed(()=>
-    this.publicacionesState().filter(publicacion => publicacion.activo === true && publicacion.estadoMascota ==='reencontrado')
+  public publicacionesReencontrados = computed(() =>
+    this.publicacionesState().filter(
+      (publicacion) => publicacion.activo === true && publicacion.estadoMascota === 'reencontrado'
+    )
   );
 
   constructor(private http: HttpClient) {
@@ -80,5 +81,28 @@ export class PublicacionService {
 
   getPublicacionById(id: number) {
     return this.http.get<Publicacion>(`${this.apiUrl}/${id}`);
+  }
+
+  // Actualiza el estado de la mascota en el backend y luego actualiza el estado local (signal).
+  updateEstadoMascota(id: number, estadoNuevo: EstadoMascota){
+    // constante con solo que se modifica
+    const payload = { estadoMascota: estadoNuevo };
+
+    return this.http.patch<Publicacion>(`${this.apiUrl}/${id}`, payload).pipe(
+      // se actualiza el signal (estado local)
+      tap((publicacionActualizada) => {
+        // la API devuelve la publicacion completa actualizada
+        this.publicacionesState.update((publicacionesActuales) => {
+          return publicacionesActuales.map((p) => {
+            // si el ID coincide, reemplazamos el objeto con la version recibida del servidor
+            if (p.id === id) {
+              return publicacionActualizada;
+            }
+            // sino se devuelve la publicacion origianl
+            return p;
+          });
+        });
+      })
+    );
   }
 }
