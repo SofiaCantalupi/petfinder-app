3d4732f351d1956dd9dec233e028cf7a385396b2
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-22 21:55:52 -0300

    add animation into hero

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,8 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { CatAndDogAnimation } from '../cat-and-dog-animation/cat-and-dog-animation';
+
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [NgClass, CatAndDogAnimation],
   templateUrl: './hero.html',
 })
 export class Hero {}
