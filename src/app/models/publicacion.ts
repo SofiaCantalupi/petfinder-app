71a7e2c0f16f2c8d18692ed43f86faee2c28d751
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-07-30 22:51:15 -0300

    Add tag for "en_adopcion" state, button to change state from "encontrado" to "en_adopcion" and button to apply for adoption

--- src/app/models/publicacion.ts
+++ src/app/models/publicacion.ts
@@ -1,5 +1,5 @@
 //Union type
-export type EstadoMascota = 'perdido' | 'encontrado' | 'reencontrado';
+export type EstadoMascota = 'perdido' | 'encontrado' | 'reencontrado' | 'en_adopcion';
 export type TipoMascota = 'perro' | 'gato';
 
 export interface Publicacion{
