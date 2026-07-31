932e2bbcba8830f8078a1137e5e26668b47854bd
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-07-30 22:47:15 -0300

    Add path for solicitud-adopcion form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MuroPublicaciones } from './pages/muro-publicaciones/muro-publicaciones';
 import { PublicacionFormComponent } from './pages/publicacion-form-component/publicacion-form-component';
 import { PublicacionDetail } from './pages/publicacion-detail/publicacion-detail';
+import { SolicitudFormComponent } from './pages/solicitud-form-component/solicitud-form-component';
 import { Login } from './pages/login/login';
 import { Registro } from './pages/registro/registro';
 import { MiPerfil } from './pages/mi-perfil/mi-perfil';
@@ -49,6 +50,11 @@ export const routes: Routes = [
   {
     path: 'publicaciones/:id/editar',
     component: PublicacionFormComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'publicaciones/:idPublicacion/solicitud-adopcion',
+    component: SolicitudFormComponent,
     canActivate: [authGuard],
   },
   {
